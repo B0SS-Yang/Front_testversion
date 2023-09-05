219c46b3faa727c456c549bfad1d89e44e6cc9fe
Arno <1945374160@qq.com>
2023-09-05 21:02:31 +1000

    添加注释

--- my-project-backend/src/main/java/com/example/config/WebConfiguration.java
+++ my-project-backend/src/main/java/com/example/config/WebConfiguration.java
@@ -11,6 +11,4 @@ public class WebConfiguration {
     BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }//这个bean是用来加密的，这里我们使用的是BCryptPasswordEncoder，它是Spring Security提供的一个密码加密工具，可以实现不可逆的加密
-
-
 }
