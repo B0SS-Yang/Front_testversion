912f28c202b73fb5cb6874fac021231d60947af5
Arno <1945374160@qq.com>
2023-09-07 23:44:08 +1000

    添加了数据库，修改Readme

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # SVlogin
+## 数据库
+### 数据库连接配置
+在./my-project-backend/src/main/resources/application.properties中修改数据库连接配置
+### 数据库建表
+建表语句为db_account.sql，
+若使用默认配置，需要在本地创建名为test的数据库
+
 ## my-project-frontend前端
 注意前端操作要在./my-project-frontend目录下进行
 ### Project Setup安装依赖
@@ -22,7 +29,7 @@ npm run build
 ## my-project-backend后端
 注意后端操作要在./my-project-backend目录下进行
 ### Project Setup安装依赖
-打开IDEA，导入项目，等待自动安装依赖
+打开IDEA，导入项目，去pom.xml中下载依赖或者等待IDEA自动下载依赖
 ### Run
 点击IDEA右上角的运行按钮，选择运行后端程序（MyProjectBackendApplication）
 
---
+++ db_account.sql
@@ -0,0 +1,36 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 本地数据库
+ Source Server Type    : MySQL
+ Source Server Version : 50737
+ Source Host           : localhost:3306
+ Source Schema         : test
+
+ Target Server Type    : MySQL
+ Target Server Version : 50737
+ File Encoding         : 65001
+
+ Date: 07/09/2023 23:36:49
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for db_account
+-- ----------------------------
+DROP TABLE IF EXISTS `db_account`;
+CREATE TABLE `db_account`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `role` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `register_time` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `unique_name`(`username`) USING BTREE,
+  UNIQUE INDEX `unique_email`(`email`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
